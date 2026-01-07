85dc60873356fa1b803e40ef7ecddeb4f74eed60
Divya Singh <divyasorrot3005@gmail.com>
2026-01-07 18:17:51 +0530

    fix: explicitly pass database url to prisma client

--- server/src/prismaClient.js
+++ server/src/prismaClient.js
@@ -3,7 +3,9 @@ const { PrismaClient } = require('@prisma/client');
 let prisma;
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+  });
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
